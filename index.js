e6b4458ce956db634bf15e42bb2c239677bf92a0
Darren Zhang <darrenzhang@Darrens-MacBook-Pro.local>
2020-09-14 10:59:26 -0400

    index and login routes

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+
+//allow cors
+var cors = require('cors')
+app.use(cors())
+
+// parse application/x-www-form-encoded and application/json
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: true
+})); app.use(bodyParser.json())
+
+//routes
+const user = require('./routes/user')
+const project = require('./routes/project')
+
+//connect to mongoose
+const mongoose = require('mongoose')
+mongoose.connect(
+    "",
+    { useNewUrlParser: true }
+)
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("connected to db")
+})
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.use('/user', user)
+app.use('/project', project)
+
+app.listen(port, () => console.log("listening at port", port))
